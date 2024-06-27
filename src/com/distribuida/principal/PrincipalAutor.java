f3d23a799ca2e0ecf26ebe04b712350e58b44738
NOCTURNO <NOCTURNO@DESKTOP-9F6MVN3>
2024-06-26 20:08:50 -0500

    EMv0013 implementacion autor y cliente

--- src/com/distribuida/principal/PrincipalAutor.java
+++ src/com/distribuida/principal/PrincipalAutor.java
@@ -13,18 +13,25 @@ public class PrincipalAutor {
 		// TODO Auto-generated method stub
 		
 		// Patron de IoC o Inversión de Conrtol
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
-
-		AutorDAO autorDAO = context.getBean("autorDAOImpl",AutorDAO.class);
-		
-		List<Autor> autor = autorDAO.findAll();
-		
-		autor.forEach(item -> {
-			
-			System.out.println(item.toString());
-		});
+				ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
+				AutorDAO autorDAO = context.getBean("autorDAOImpl2",AutorDAO.class);
 				
-		context.close();
+				//CRUD : CREATE, READ UPDATE Y DELETE
+				//ADD
+				Autor Autor = new Autor(0,"juan","taipe","pais","direccion","098764321","jtaipe@correo.com");
+				//autorDAO.add(Autor); //crear
+				//up
+				Autor Autor2 = new Autor(54,"juan2","taipe2","pais2","direccion2","0987643212","jtaipe2@correo.com");
+				autorDAO.up(Autor2); //modificar 
+				//del
+				//autorDAO.del(40);
+				//findOne
+				System.out.print("*****************DEL********************"+autorDAO.findOne(40));
+				//findAll	
+				//List<Autor> Autors = autorDAO.findAll();
+				autorDAO .findAll().forEach(item -> { System.out.println(item.toString()); });
+				
+				context.close();
 		
 	}
 
--- src/com/distribuida/principal/PrincipalCliente.java
+++ src/com/distribuida/principal/PrincipalCliente.java
@@ -14,16 +14,23 @@ public class PrincipalCliente {
 		
 		// Patron de IoC o Inversión de Conrtol
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
-
 		ClienteDAO clienteDAO = context.getBean("clienteDAOImpl",ClienteDAO.class);
 		
-		List<Cliente> clientes = clienteDAO.findAll();
+		//CRUD : CREATE, READ UPDATE Y DELETE
+		//ADD
+		Cliente cliente = new Cliente(40,"1789456123","juan","taipe","098764321","direccion","jtaipe@correo.com");
+		clienteDAO.add(cliente);
+		//up
+		Cliente cliente2 = new Cliente(40,"17894561232","juan2","taipe2","0987643212","direccion2","jtaipe2@correo.com");
+		//clienteDAO.add(cliente2);
+		//del
+		clienteDAO.del(40);
+		//findOne
+		System.out.print("*****************DEL********************"+clienteDAO.findOne(40));
+		//findAll	
+		//List<Cliente> clientes = clienteDAO.findAll();
+		clienteDAO .findAll().forEach(item -> { System.out.print(item.toString()); });
 		
-		clientes.forEach(item -> {
-			
-			System.out.println(item.toString());
-		});
-				
 		context.close();
 		
 	}
